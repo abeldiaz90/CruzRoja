bb95da499cfebb81639e91415231ff157a04b454
abeldiaz90 <abeldiaz90@hotmail.com>
2020-07-11 13:30:59 -0500

    Upload Fonts

---
+++ mvc/Models/ServiciosAmbulanciasVistas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc.Models
+{
+    public class ServiciosAmbulanciasVistas
+    {
+        public ServiciosAmbulancias serviciosAmbulancias { set; get; }
+        public Colonias colonias { set; get; }
+        public Claves claves { set; get; }
+        public Ambulancias ambulancias { set; get; }
+    }
+}
